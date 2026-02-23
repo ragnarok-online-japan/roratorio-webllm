0bd5906b978de0fa97ef6a6ff4a00473c537adbb
MINETA "m10i" Hiroki <h-mineta@0nyx.net>
2026-02-23 22:46:44 +0900

    fix: Fixed base

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-    base: '/webllm-vanilla-framework/',
+    base: '/',
     build: {
         outDir: 'dist',
         emptyOutDir: true,
